7cc6d9bdcb95250135fc44d5c926837c07a6f9d8
Maki Aboabida <maki-miko@hotmail.com>
2021-09-23 17:12:54 -0400

    added regular expressions to tests

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -10,13 +10,15 @@ test('checks if creates Employee class', () => {
 
 // checks for employee name
 test('checks for name in Employee class', () => {
-    const employee = new Employee('Maki')
+    const employee = new Employee('Maki Abo Maki')
+    const regEmployee = /^[a-zA-Z,.'-]+([a-zA-Z ,.'-]+)([a-z]+$)/
 
-    expect(employee.name).toEqual(expect.any(String))
+    // expect(employee.name).toEqual(expect.any(String))
+    expect(employee.name).toMatch(regEmployee)
 })
 
 // checks for id of employee 
-test('checks for employee ID in Employee class', () => {
+test('checks for employee ID Number in Employee class', () => {
     const employee = new Employee('Maki', 1)
 
     expect(employee.id).toEqual(expect.any(Number))
@@ -25,11 +27,11 @@ test('checks for employee ID in Employee class', () => {
 
 // checks for email in Employee class 
 test('checks email in Employee class', () => {
-    const employee = new Employee('Maki', 3, 'maki-miko@hotmail.com')
+    const employee = new Employee('Maki', 3, 'maki_miko@hotmail.com')
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    // words .OR- words @ .OR-Words . 2-3 letters
 
-    expect(employee.email).toEqual(expect.any(String))
-    expect(employee.email).toEqual(expect.stringContaining('@'))
-    expect(employee.email).toEqual(expect.stringContaining('.'))
+    expect(employee.email).toMatch(regexEmail)
 })
 
 // checking getName function in Employee class 
@@ -44,16 +46,16 @@ test('checks for getId() in Employee class', () => {
     const employee = new Employee('Maki', 1, 'maki-miko@hotmail.com')
 
     expect(employee.getId()).toEqual(expect.any(Number))
-    console.log(employee.getId())
 })
 
 // gets email() from Employee class 
 test('checks for getEmail() method in Employee class', () => {
     const employee = new Employee('Maki', 1, 'maki-miko@hotmail.com')
+    const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-    expect(employee.getEmail()).toEqual(expect.any(String))
-    expect(employee.getEmail()).toEqual(expect.stringContaining('@'))
-    expect(employee.getEmail()).toEqual(expect.stringContaining('.'))
+    expect(employee.getEmail()).toEqual('maki-miko@hotmail.com')
+    expect(employee.getEmail()).toMatch(regexEmail)
+
 })
 
 // getting constructor name for the role 
--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -14,9 +14,11 @@ test('check if Engineer.js exports constructor from Employee.js', () => {
 
 test('check github username in Manager class', () => {
     const engineer = new Engineer('Maki', 1, 'maki-miko@hotmail.com', 'Makispear')
+    const gitHubRegEx = /^[a-z]+[a-z-][a-z]+$/i
 
     expect(engineer.githubUsername).toEqual(expect.any(String))
     expect(engineer.githubUsername.length).toBeLessThanOrEqual(39)
+    expect(engineer.githubUsername).toMatch(gitHubRegEx)
 })
 
 test('checks for getGithub() in Engineer class', () => {
