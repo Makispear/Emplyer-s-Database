9070258594e743625e7c22d88059ede4cce96095
Maki Aboabida <maki-miko@hotmail.com>
2021-09-23 01:42:35 -0400

    added email property with failed, then succeeded test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -10,7 +10,7 @@
 const Employee = require('../lib/Employee.js')
 
 // checks if creates is an object
-test('Creates and employee object', () => {
+test('checks if creates Employee class', () => {
     const employee = new Employee('Maki')
 
     expect(employee.name).toEqual(expect.any(String))
@@ -18,22 +18,31 @@ test('Creates and employee object', () => {
 
 
 // checks for employee name
-test('checks for employee name', () => {
+test('checks for name in Employee class', () => {
     const employee = new Employee('Maki')
 
     expect(employee.name).toEqual(expect.any(String))
 })
 
 // checks for id of employee 
-test('checks for employee ID', () => {
+test('checks for employee ID in Employee class', () => {
     const employee = new Employee('Maki', 1)
 
     expect(employee.id).toEqual(expect.any(Number))
     expect(employee.id).toBeGreaterThan(0)
 })
 
+// checks for email in Employee class 
+test('checks email in Employee class', () => {
+    const employee = new Employee('Maki', 3, 'maki-miko@hotmail.com')
+
+    expect(employee.email).toEqual(expect.any(String))
+    expect(employee.email).toEqual(expect.stringContaining('@'))
+    expect(employee.email).toEqual(expect.stringContaining('.'))
+})
+
 // checking getName function in Employee class 
-test('Check for getName() in Employee Class', () => {
+test('Check for getName() method in Employee Class', () => {
     const employee = new Employee('Maki', 1)
 
     expect(employee.getName()).toEqual('Maki')
