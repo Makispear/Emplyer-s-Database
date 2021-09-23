cad7cecaa0103bcf67ce42f9a589f479a566bfa6
Maki Aboabida <maki-miko@hotmail.com>
2021-09-23 00:20:50 -0400

    added name property for Employee

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -9,6 +9,14 @@
 
 const Employee = require('../lib/Employee.js')
 
+// checks if creates is an object
+test('Creates and employee object', () => {
+    // const employee = new Employee('Maki')
+
+    expect(Employee.name).toEqual(expect.any(String))
+})
+
+// checks for employee name
 test('checks for employee name', () => {
     const employee = new Employee('Maki')
 
