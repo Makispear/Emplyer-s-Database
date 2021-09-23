636484c808008411e6a1cc1b3ab85b1051eaefaa
Maki Aboabida <maki-miko@hotmail.com>
2021-09-23 00:24:47 -0400

    fixed last error

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -11,14 +11,15 @@ const Employee = require('../lib/Employee.js')
 
 // checks if creates is an object
 test('Creates and employee object', () => {
-    // const employee = new Employee('Maki')
+    const employee = new Employee('Maki')
 
-    expect(Employee.name).toEqual(expect.any(String))
+    expect(employee.name).toEqual(expect.any(String))
 })
+
 
 // checks for employee name
 test('checks for employee name', () => {
     const employee = new Employee('Maki')
 
-    expect(Employee.name).toEqual(expect.any(String))
+    expect(employee.name).toEqual(expect.any(String))
 })
